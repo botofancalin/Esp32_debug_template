a1ec36d033a92d59b5fe981344e58b228333d2c8
Calin <botofancalin@gmail.com>
2019-04-15 02:04:17 +0300

    Working debug

--- main/blink.c
+++ main/blink.c
@@ -28,13 +28,14 @@ void app_main()
     gpio_pad_select_gpio(BLINK_GPIO);
     /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-    while(1) {
+    while (1)
+    {
         /* Blink off (output low) */
-	printf("Turning off the LED\n");
+        printf("Turning off the LED\n");
         gpio_set_level(BLINK_GPIO, 0);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
         /* Blink on (output high) */
-	printf("Turning on the LED\n");
+        printf("Turning on the LED\n");
         gpio_set_level(BLINK_GPIO, 1);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
